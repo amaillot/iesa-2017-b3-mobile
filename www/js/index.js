581b49431068ea2e1cc3fefbdfb3eb3117f0a950
amaillot <amaillot01@gmail.com>
2017-04-20 16:57:41 +0200

    plugin network information

--- www/js/index.js
+++ www/js/index.js
@@ -30,6 +30,23 @@ var app = {
         this.receivedEvent('deviceready');
         var element = document.getElementById('device');
         element.innerHTML = '<ul><li>'+device.model+'</li><li>'+device.platform+'</li><li>'+device.cordova+'</li><ul>';
+        function checkConnection() {
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
+
+            alert('Connection type: ' + states[networkState]);
+        }
+
+        checkConnection();
     },
 
     // Update DOM on a Received Event
